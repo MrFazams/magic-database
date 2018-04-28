73884983df9eb9dd6192718ac9550abb80d02374
MrFazams <38036673+MrFazams@users.noreply.github.com>
2018-04-28 21:41:16 +0300

    Add files via upload
    Bug fix

--- Container.h
+++ Container.h
@@ -5,39 +5,49 @@ template <typename T>
 class Container
 {
 
-	int size;
-	int capacity;
+	unsigned int size;
+	unsigned int capacity;
 	T * elements;
 
 	void resizeUp() {
-		T * temp = elements;
-		elements = new T[2 * capacity];
-		for (int i = 0; i < size; i++) {
-			elements[i] = temp[i];
+		if (capacity == 0) {
+			++capacity;
+			this->elements = new T[1];
 		}
-		capacity *= 2;
-		delete[] temp;
+		else {
+			T * temp = elements;
+			elements = new T[2 * capacity];
+			for (unsigned int i = 0; i < size; i++) {
+				elements[i] = temp[i];
+			}
+			capacity *= 2;
+			delete[] temp;
+		}
 	}
 
 public:
 
-	Container(int capacity = 1, int size = 0, T * elements = NULL) {
+	Container(unsigned int capacity = 1, unsigned int size = 0, T * elements = NULL) {
 		this->capacity = capacity;
 		if (size > capacity) this->size = capacity;
 		else this->size = size;
-		if (elements != NULL && capacity != 0) for (int i = 0; i < size; i++) add(elements[i]);
+		if (elements != NULL && capacity > 0) for (unsigned int i = 0; i < size; i++) add(elements[i]);
+		else this->elements = NULL;
 	}
 
 	Container(const Container & c) {
 		*this = c;
 	}
-	~Container();
 
-	int getSize() const {
+	~Container() {
+		delete[] elements;
+	}
+
+	unsigned int getSize() const {
 		return size;
 	}
 
-	int getCapacity() const {
+	unsigned int getCapacity() const {
 		return capacity;
 	}
 
@@ -50,7 +60,8 @@ public:
 	const Capacity & operator=(const Capacity & c) {
 		this->capacity = c.getCapacity();
 		this->size = c.getSize();
-		if (c.getSize() > 0) for (int i = 0; i < c.getSize(); i++) add(c[i]);
+		if (c.getSize() > 0) for (unsigned int i = 0; i < c.getSize(); i++) add(c[i]);
+		else this->elements = NULL;
 	}
 
 	T & operator[](unsigned int idx) {
